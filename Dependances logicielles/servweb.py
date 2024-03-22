024d7cc55ac82849d09a985babc12ad5021f3981
raphael-attias <137683618+raphael-attias@users.noreply.github.com>
2024-03-22 13:41:26 +0100

    mise à jour de sécurisation

--- Dependances logicielles/servweb.py
+++ Dependances logicielles/servweb.py
@@ -7,27 +7,27 @@ def installer_paquets():
     
     serveur_web = input("Voulez-vous installer Apache (A/a) ou Nginx (N/n) ? : ")
     if serveur_web.lower() == "a":
-        subprocess.run(["sudo", "apt", "install", "apache2", "-y"])
+        subprocess.run(["apt", "install", "apache2", "-y"])
     elif serveur_web.lower() == "n":
-        subprocess.run(["sudo", "apt", "install", "nginx", "-y"])
+        subprocess.run(["apt", "install", "nginx", "-y"])
     else:
         print("Choix non valide. Installation d'Apache par défaut.")
-        subprocess.run(["sudo", "apt", "install", "apache2", "-y"])
+        subprocess.run(["apt", "install", "apache2", "-y"])
 
-    subprocess.run(["sudo", "apt", "install", "phpmyadmin", "-y"])
+    subprocess.run(["apt", "install", "phpmyadmin", "-y"])
 
     base_de_donnees = input("Voulez-vous installer MySQL (M/m) ou MariaDB (R/r) ? : ")
     if base_de_donnees.lower() == "m":
-        subprocess.run(["sudo", "apt", "install", "mysql-server", "-y"])
+        subprocess.run(["apt", "install", "mysql-server", "-y"])
     elif base_de_donnees.lower() == "r":
-        subprocess.run(["sudo", "apt", "install", "mariadb-server", "-y"])
+        subprocess.run(["install", "mariadb-server", "-y"])
     else:
         print("Choix non valide. Installation de MySQL par défaut.")
-        subprocess.run(["sudo", "apt", "install", "mysql-server", "-y"])
+        subprocess.run(["apt", "install", "mysql-server", "-y"])
 
-    subprocess.run(["sudo", "apt", "install", "nodejs", "npm", "-y"])
+    subprocess.run(["apt", "install", "nodejs", "npm", "-y"])
 
-    subprocess.run(["sudo", "apt", "install", "git", "-y"])
+    subprocess.run(["apt", "install", "git", "-y"])
 
     print("Installation des paquets terminée.")
 
--- Maj automatique/majlo.sh
+++ Maj automatique/majlo.sh
@@ -2,7 +2,7 @@
 read -p "Voulez-vous mettre  jour les logiciels ? (O/n) : " choix
 #choix de l'utilisateur
 if [[ $choix == "O" || $choix == "o" || $choix == "" ]]; then
-	sudo apt update && sudo apt upgrade -y
+	apt update && apt upgrade -y
 	if [ $? -eq 0 ]; then
 		echo "Maj des logiciels fait !"
 	else
